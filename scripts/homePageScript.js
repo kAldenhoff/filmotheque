b037c56dfa5232ea21e53b3882e0272be3f5fff5
Kevin Aldenhoff <kevin.aldenhoff@gmail.com>
2018-11-17 16:34:56 +0100

    title and synopsis in overlay

--- scripts/homePageScript.js
+++ scripts/homePageScript.js
@@ -1,14 +1,19 @@
 
   function displayDetails(details){
-     document.getElementById("detailsPoster").src = "https://image.tmdb.org/t/p/w342/"+details.poster_path;
 
-     var movieTitle = document.getElementById("movieInfos").getElementsByTagName("h1")[0];
-     movieTitle.innerText = details.title+" ";
+    document.getElementsByClassName("overlay")[0].style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.9),rgba(0, 0, 0, 0.9)),url('https://image.tmdb.org/t/p/w1280/${details.backdrop_path}')`;
 
-     var releaseYear = new Date(details.release_date);
-     releaseYear = releaseYear.getFullYear();
-     movieTitle.appendChild(document.createElement("span"));
-     movieTitle.getElementsByTagName("span")[0].innerText = "("+releaseYear+")";
+    document.getElementById("detailsPoster").src = "https://image.tmdb.org/t/p/w342/"+details.poster_path;
+
+    var movieTitle = document.getElementById("movieInfos").getElementsByTagName("h1")[0];
+    movieTitle.innerText = details.title+" ";
+
+    var releaseYear = new Date(details.release_date);
+    releaseYear = releaseYear.getFullYear();
+    movieTitle.appendChild(document.createElement("span"));
+    movieTitle.getElementsByTagName("span")[0].innerText = "("+releaseYear+")";
+
+    document.getElementById("synopsis").innerText = details.overview;
   }
 
 
